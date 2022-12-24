41b80623095c86a6aabf0c56b667beef2b87c0c0
Alexis Vaquero <alexisvaquero@hotmail.com>
2022-12-23 20:56:53 -0600

    init-proyect

---
+++ app/src/main/java/com/example/ejercicio2_starwarsapi/Movies.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicio2_starwarsapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Movies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+    }
+}
